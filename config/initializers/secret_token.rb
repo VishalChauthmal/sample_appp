6ed084f635fbb32475284d450f26df8f1aef682e
Vishal25 <vishalchauthmal.com>
2014-05-21 16:50:46 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleAppp::Application.config.secret_key_base = '50392e845af50a31859840c4c8f940d0b5de2fcb789608dd87b8a9c0480c18faa9c90777ea513baabbf9d3af2fef54a58840fc8103e762ce30d16aa12c531dcd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SampleAppp::Application.config.session_store :cookie_store, key: '_sample_appp_session'
