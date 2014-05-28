9de19f5c46b1cce19ba605ab99228fdc7cb849bc
Vishal25 <vishalchauthmal.com>
2014-05-28 22:28:39 +0530

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,8 @@ describe User do
 	it { should respond_to(:password) }
 	it { should respond_to(:password_confirmation) }
 
+	it { should respond_to(:remember_token) }
+
 	it { should be_valid }
 
 	describe "when name is not present" do
@@ -96,5 +98,10 @@ describe User do
 			it { should_not eq user_for_invalid_password }
 			specify { expect(user_for_invalid_password).to be_false }
 		end
+	end
+
+	describe "remember token" do
+		before { @user.save }
+		its(:remember_token) { should_not be_blank }
 	end
 end
