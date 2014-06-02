bb69d6998579c005478420f50650bd2eaffa0380
Vishal25 <vishalchauthmal.com>
2014-06-02 05:32:18 +0530

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleAppp::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
   
   root 'static_pages#home'
   match '/help', to: 'static_pages#help', via: 'get'
